72ed4148c65f7f12de8220a9429695b5ee1877a6
KnezzM <knezevicmilan4@gmail.com>
2026-04-22 22:18:22 -0400

    Added Canvas creation and sticky notes dragging

--- frontend/main.js
+++ frontend/main.js
@@ -1,18 +1,17 @@
-const { create } = require("domain");
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 1000,
-        height: 700,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
 
-    win.loadFile(path.join(__dirname, "index.html"));
+  win.loadFile(path.join(__dirname, "index.html"));
 }
 
 app.whenReady().then(() => {
