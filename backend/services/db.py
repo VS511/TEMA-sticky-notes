77c767ca165d70cfdef46f34c19e02f7e6e4a547
Alejandro <alejandrociuba@gmail.com>
2026-04-24 03:59:08 -0400

    Finished basic working code for CodeDataService in db.py

--- backend/services/db.py
+++ backend/services/db.py
@@ -5,6 +5,9 @@ import psycopg2
 
 class CodeDataService:
 
+    _columns: list[str] = ["id", "codeid", "collection", "text", "color", "position"]
+    _text_columns: list[str] = ["collection", "text"]
+
     def __init__(self, canvas_name: str = None):
 
         if canvas_name is None:
@@ -30,8 +33,9 @@ class CodeDataService:
 
         self.cur.execute(f"""CREATE TABLE IF NOT EXISTS {self.canvas_name} (
                          id SERIAL PRIMARY KEY,
-                         group VARCHAR(100),
-                         text VARCHAR(50),
+                         codeid INT UNIQUE,
+                         collection VARCHAR(100),
+                         text VARCHAR(1000),
                          color INT,
                          position POINT
                          );
@@ -43,23 +47,91 @@ class CodeDataService:
         self.cur.close()
         self.conn.close()
 
-    def create_code_entry() -> int:
-        pass
+    def create_code_entry(self, **kwargs) -> int:
 
-    def delete_code_entry() -> int:
-        pass
+        # Create entry strings dynamically based on the provided data for the insertion
+        entry_string: str = f"{','.join([entry for entry in kwargs if entry in self._columns])}"
+        data_list: list[str] = []
+        for entry in kwargs:
+            if entry in self._text_columns:
+                data_list.append(f"'{kwargs[entry]}'")
+            elif entry == "position":
+                data_list.append(f"point{str(kwargs[entry])}")
+            elif entry in self._columns:
+                data_list.append(f"{str(kwargs[entry])}")
 
-    def update_code_entry():
-        pass
+        data_string: str = ",".join(data_list)
 
-    def get_code_by_id() -> tuple:
-        pass
+        self.cur.execute(f"""INSERT INTO {self.canvas_name} ({entry_string}) VALUES
+                         ({data_string});""")
+        
+        self.cur.execute(f"""SELECT * FROM {self.canvas_name}
+                    WHERE codeid = {kwargs["codeid"]};""")
+        
+        return self.cur.fetchone()[0]
 
-    def get_groups() -> list[str]:
-        pass
+    def delete_code_entry(self, codeid: int = None):
 
-    def fetch_codes() -> list[tuple]:
-        pass
+        if codeid is None:
+            TypeError("id is of type None")
+        if codeid is not None:
+            if codeid < 0:
+                ValueError("Invalid id")
+
+        self.cur.execute(f"""DELETE FROM {self.canvas_name}
+                         WHERE codeid = {codeid};""")
+
+    def update_code_entry(self, **kwargs):
+
+        if "codeid" not in kwargs.keys():
+            raise KeyError("codeid missing")
+
+        # Generate the SET string dynamically based on the provided data
+        data_list: list[str] = []
+        for entry in kwargs:
+            if entry in self._text_columns:
+                data_list.append(f"{entry} = '{kwargs[entry]}'")
+            elif entry == "position":
+                data_list.append(f"{entry} = point{str(kwargs[entry])}")
+            elif entry in self._columns and entry not in ["id", "codeid"]:
+                data_list.append(f"{entry} = {str(kwargs[entry])}")
+
+        data_string: str = ",\n\t".join(data_list)
+
+        self.cur.execute(f"""UPDATE {self.canvas_name}
+                         SET
+                            {data_string}
+                         WHERE codeid = {kwargs["codeid"]};""")
+
+    def get_code_by_id(self, id: int = None) -> tuple | None:
+
+        if id is None:
+            raise TypeError("id is None")
+        if id is not None:
+            if id < 0:
+                raise ValueError("Invalid id")
+
+        self.cur.execute(f"""SELECT * FROM {self.canvas_name}
+                         WHERE id = {id};""")
+        return self.cur.fetchall()
+
+    def get_code_by_codeid(self, codeid: int = None) -> tuple | None:
+
+        if codeid is None:
+            raise TypeError("codeid is None")
+
+        self.cur.execute(f"""SELECT * FROM {self.canvas_name}
+                         WHERE codeid = {codeid};""")
+        return self.cur.fetchall()
+
+    def get_collections(self) -> list[str] | None:
+        self.cur.execute(f"""SELECT DISTINCT collection
+                         FROM {self.canvas_name};""")
+        return [c[0] for c in self.cur.fetchall()]
+
+    def fetch_codes(self) -> list[tuple]:
+        self.cur.execute(f"""SELECT * FROM {self.canvas_name}""")
+        return self.cur.fetchall()
 
 
 class CanvasDataService:
@@ -207,6 +279,7 @@ class CanvasDataService:
 
 
 if __name__ == "__main__":
+
     with CanvasDataService() as c:
         print(c.delete_canvas("test"))
         print(c.create_canvas("test"))
@@ -215,3 +288,33 @@ if __name__ == "__main__":
         print(c.get_canvas_name(c.get_canvas_id("test")))
         print(c.delete_canvas("test"))
         print(c.fetch_canvases())
+
+
+    with CanvasDataService() as c:
+        print(c.delete_canvas("codetest"))
+        print(c.create_canvas("codetest"))
+
+    entry = {
+        "text": "This is a test code",
+        "color": 0,
+        "collection": "Test",
+        "position": (2.0, 4.5),
+        "codeid": 5,
+    }
+
+    update_entry = {
+        "text": "This is an updated test code",
+        "position": (2.0, 3.5),
+        "codeid": 5,
+    }
+
+    with CodeDataService(canvas_name="codetest") as codes:
+        print(codes.delete_code_entry(5))
+        print(codes.create_code_entry(**entry))
+        print(codes.fetch_codes())
+        print(codes.update_code_entry(**update_entry))
+        print(codes.fetch_codes())
+        print(codes.get_code_by_codeid(5))
+        print(codes.get_code_by_codeid(1))
+        print(codes.get_code_by_id(100))
+        print(codes.get_collections())
